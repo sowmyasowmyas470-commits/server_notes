b0a46731bf15195ff22513b5b9c9e3f745d9f85b
sowmyasowmyas470-commits <sowmyasowmyas470@gmail.com>
2026-04-22 16:01:07 +0530

    first commit

--- data_base/connection.js
+++ data_base/connection.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.log("Error connecting to MongoDB:", error);
-    }
-};
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error( error);
+  }
+}
 
 export default connectDB;
